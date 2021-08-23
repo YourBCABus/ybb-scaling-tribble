a0ad27373d8c7c65de5e97bed185fe74185dfaf3
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2021-08-23 17:31:57 -0400

    Change saveBoardingAreaCallback to default to "?" on an empty string.

--- lib/editingCallbacks.ts
+++ lib/editingCallbacks.ts
@@ -4,7 +4,7 @@ export const saveBoardingAreaCallback =
     (updateServerSidePropsFunction: () => void) => 
         (id: string) => 
             async (boardingArea: string | null): Promise<void> => {
-                if (boardingArea == null) {
+                if (!boardingArea) {
                     boardingArea = "?";
                 }
                 await fetch(`/api/updateBusStatus?id=${encodeURIComponent(id)}&boardingArea=${encodeURIComponent(boardingArea)}`);

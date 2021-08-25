94cab517cdd6c7ae6b4f912e21be60581e8f3d75
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2021-08-25 18:12:05 -0400

    Make / redirect to the BCA school page.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
     reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/school/5bca51e785aa2627e14db459",
+                permanent: true,
+            },
+        ];
+    },
 };

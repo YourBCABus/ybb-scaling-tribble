607c4ebeeef7e60e66e97f3da865b41eb614dba4
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2021-09-11 22:57:13 -0400

    Create api route for clear all mutation.

---
+++ __generated__/ClearAll.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: ClearAll
+// ====================================================
+
+export interface ClearAll {
+  clearAll: boolean;
+}
+
+export interface ClearAllVariables {
+  schoolID: string;
+}
